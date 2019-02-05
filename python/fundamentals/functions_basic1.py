eba9a5325b00e3231450ba0c6b75572d4a7aa6a2
alextle0125 <alextle0125>
2019-02-05 13:06:42 -0800

    finished functions_basic1.py

---
+++ python/fundamentals/functions_basic1.py
@@ -0,0 +1,168 @@
+#1
+def a():
+    return 5
+print(a())
+
+# Returns 5
+
+#2
+def a():
+    return 5
+print(a()+a())
+
+# Returns 10
+
+#3
+def a():
+    return 5
+    return 10
+print(a())
+
+# Returns 5
+
+#4
+def a():
+    return 5
+    print(10)
+print(a())
+
+# Returns 5
+
+#5
+def a():
+    print(5)
+x = a()
+print(x)
+
+# Prints 5; Returns None
+
+#6
+def a(b,c):
+    print(b+c)
+print(a(1,2) + a(2,3))
+
+# Prints 3
+# Prints 5
+# TypeError: unsupported operand type(s) for +: 'NoneType' and 'NoneType'
+
+#7
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5))
+
+# 25
+
+#8
+def a():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a())
+
+# Prints 100
+# Prints 10
+
+#9
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3))
+print(a(5,3))
+print(a(2,3) + a(5,3))
+
+# Prints 7
+# Prints 14
+# Prints 21
+
+#10
+def a(b,c):
+    return b+c
+    return 10
+print(a(3,5))
+
+# Print 8
+
+#11
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+print(b)
+a()
+print(b)
+
+# Prints 500
+# Prints 500
+# Prints 300
+# Prints 500
+
+#12
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+a()
+print(b)
+
+# Prints 500
+# Prints 500
+# Prints 300
+# Prints 500
+
+#13
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=a()
+print(b)
+
+# Prints 500
+# Prints 500
+# Prints 300
+# Prints 300
+
+#14
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a()
+
+# Prints 1
+# Prints 3
+# Prints 2
+
+#15
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
+
+# Prints 1
+# Prints 3
+# Prints 5
+# Prints 10
+
