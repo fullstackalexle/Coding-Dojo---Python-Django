c8d2dd2296ebe073f50a7c3673fb3016a755128d
alextle0125 <alextle0125>
2019-02-05 12:29:41 -0800

    finished for_loop_basic1.py

---
+++ python/fundamentals/for_loop_basic1.py
@@ -0,0 +1,24 @@
+# for i in range(151):
+# 	print(i)
+
+# for i in range(5,1001,5):
+# 	print(i)
+
+# for i in range(101):
+# 	if i % 10 == 0:
+# 		print("Coding Dojo")
+# 	elif i % 5 == 0:
+# 		print("Coding")
+# 	else:
+# 		print(i)
+
+# print(sum(i for i in range(500001) if i % 2 != 0))
+
+# for i in range(2018,-1,-4):
+# 	print(i)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range(lowNum,highNum,mult):
+	print(i+1)
